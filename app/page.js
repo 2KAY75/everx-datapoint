f7c5c846b5c66ddfd3bb496a545f419f9af39015
2KAY75 <austinkellyy75@gmail.com>
2026-08-18 15:01:38 +0100

    Create initial Page component with content

---
+++ app/page.js
@@ -0,0 +1,4 @@
+'use client'
+export default function Page(){
+  return <div style={{padding:20}}><h1 style={{color:'#0A4FFF'}}>Ever_X Datapoint</h1><p>Live! Data, Electricity, Cable, Airtime, Education</p><p>VTU.ng + Paystack</p></div>
+}
