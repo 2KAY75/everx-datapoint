cb6fcdf1fa7d3f52242392319417172849047251
2KAY75 <austinkellyy75@gmail.com>
2026-08-18 14:57:23 +0100

    Delete app/layout.js

--- app/layout.js
+++
@@ -1,7 +0,0 @@
-export const metadata = { 
-  title: 'Ever_X Datapoint', 
-  description: 'Affordable Data, Anytime' 
-}
-export default function RootLayout({ children }) { 
-  return (<html lang="en"><body style={{margin:0}}>{children}</body></html>) 
-}
